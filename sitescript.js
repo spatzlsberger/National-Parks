8198a2c38ba1b290c94c1f34f4d457a7e80e7302
spatzlsberger <spatzlsberger@gmail.com>
2019-07-30 14:07:04 -0500

    stage index, gitignore, and sitescript

--- sitescript.js
+++ sitescript.js
@@ -1,3 +1,22 @@
-$(document).ready(function () {
-    d3.selectAll('p').style('color', 'blue');
+var data;
+
+$(document).ready(async function () {
+
+    data = await d3.csv('https://spatzlsberger.github.io/National-Parks/datasource.csv').then(function (data) {
+
+        console.log(data);
+
+        d3.select('#graph1').selectAll('rect').data(data)
+            .enter().append('rect')
+            .attr("width", 19)
+            .attr('height', 20)
+            .attr('x', function (d, i) { return 20 * i; })
+            .attr('y', function (d, i) { return 20 * i; })
+            .style('fill', 'blue');
+
+        
+    });
 });
+
+
+
