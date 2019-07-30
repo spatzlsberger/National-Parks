756dd860c168ec03b03bb27977792b7e86fa1eea
spatzlsberger <spatzlsberger@gmail.com>
2019-07-30 14:53:56 -0500

    Continued work

--- sitescript.js
+++ sitescript.js
@@ -2,21 +2,25 @@ var data;
 
 $(document).ready(async function () {
 
-    data = await d3.csv('https://spatzlsberger.github.io/National-Parks/datasource.csv').then(function (data) {
+    dataset = await d3.csv('https://spatzlsberger.github.io/National-Parks/datasource.csv').then(function (dataset) {
 
-        console.log(data);
-
-        d3.select('#graph1').selectAll('rect').data(data)
-            .enter().append('rect')
+        d3.select('#graph1').selectAll('rect').data(dataset)
+            .enter()
+            .append('rect')
             .attr("width", 19)
             .attr('height', 20)
             .attr('x', function (d, i) { return 20 * i; })
             .attr('y', function (d, i) { return 20 * i; })
             .style('fill', 'blue');
 
-        
+        data = dataset;
     });
+    
 });
 
+function clearElement(elementId) {
 
-
+    d3.select(elementId).selectAll("*").remove();
+    console.log('graph 1 cleared');
+ 
+}
